fa65dbdbe07a8d5393ad00260a9a245e9f06f793
森 真輝人 <mkt.it.engr@gmail.com>
2023-11-01 10:58:31 +0900

    Revert "chap8完了 Static and Dynamic Rendering"
    This reverts commit 7c1aa87980acc0425af9740e14841ca5f2a322f6.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -8,25 +8,22 @@ import {
   User,
   Revenue,
 } from "./definitions";
-
-import { unstable_noStore as noStore } from "next/cache";
 import { formatCurrency } from "./utils";
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
   // This is equivalent to in fetch(..., {cache: 'no-store'}).
-  noStore();
 
   try {
-    // 3秒間の遅延をシミュレートする
-    // Artificially delay a response for demo purposes.
+    // Artificially delay a reponse for demo purposes.
     // Don't do this in real life :)
-    console.log("Fetching revenue data...");
-    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    console.log("Data fetch complete after 3 seconds.");
+    // console.log('Data fetch complete after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -36,7 +33,6 @@ export async function fetchRevenue() {
 }
 
 export async function fetchLatestInvoices() {
-  noStore();
   try {
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
@@ -57,9 +53,6 @@ export async function fetchLatestInvoices() {
 }
 
 export async function fetchCardData() {
-  //静的レンダリングをオプトアウトする
-  noStore();
-
   try {
     // You can probably combine these into a single SQL query
     // However, we are intentionally splitting them to demonstrate
@@ -99,9 +92,6 @@ export async function fetchFilteredInvoices(
   query: string,
   currentPage: number
 ) {
-  //静的レンダリングをオプトアウトする
-  noStore();
-
   const offset = (currentPage - 1) * ITEMS_PER_PAGE;
 
   try {
@@ -134,9 +124,6 @@ export async function fetchFilteredInvoices(
 }
 
 export async function fetchInvoicesPages(query: string) {
-  //静的レンダリングをオプトアウトする
-  noStore();
-
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
@@ -158,9 +145,6 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
-  //静的レンダリングをオプトアウトする
-  noStore();
-
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -203,9 +187,6 @@ export async function fetchCustomers() {
 }
 
 export async function fetchFilteredCustomers(query: string) {
-  //静的レンダリングをオプトアウトする
-  noStore();
-
   try {
     const data = await sql<CustomersTable>`
 		SELECT
